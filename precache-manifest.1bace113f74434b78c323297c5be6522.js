59e5153792f3f259fc7e14c36bfafda9607ff067
Karthik Moulya <karthik@pop-os.localdomain>
2020-07-31 19:47:37 +0530

    Updates

---
+++ precache-manifest.1bace113f74434b78c323297c5be6522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8912d0590e64c219530261ece6994414",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52bae1756e55298da3fc",
+    "url": "/monsters-rolodex/static/css/main.408ef7da.chunk.css"
+  },
+  {
+    "revision": "e1ac61fa466c2396d439",
+    "url": "/monsters-rolodex/static/js/2.fe564023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fe564023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52bae1756e55298da3fc",
+    "url": "/monsters-rolodex/static/js/main.ec3aa810.chunk.js"
+  },
+  {
+    "revision": "40be79e4566793ee2bc9",
+    "url": "/monsters-rolodex/static/js/runtime-main.cb85336a.js"
+  }
+]);
